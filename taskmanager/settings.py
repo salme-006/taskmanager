c9561969d79fdb9c84ec458954bdbb44ab21dc80
salme-006 <salmeh.nemat@gmail.com>
2025-01-25 14:45:06 +0330

    backup bootstrap

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os  # اگر این خط وجود نداره، اضافه کن
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,8 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_URL = '/static/'  # مسیر عمومی فایل‌های استاتیک
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # دایرکتوری فایل‌های استاتیک
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # مسیر جمع‌آوری فایل‌ها در سرور
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -3,6 +3,8 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
+console.log('scripts.js is loaded!');
+
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
     const mainNav = document.getElementById('mainNav');
